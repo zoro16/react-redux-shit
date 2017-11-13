86eb5a47cebafb947e967a6c281142d85659cf8e
Mohamed Saleh <mohamed.saleh16@gmail.com>
2017-11-13 16:41:57 +0800

    indentation and missing semicolons

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -11,5 +11,5 @@ const App = () => {
           <Footer />
         </div>
     );
-}
+};
 export default App;
--- todo-app/src/components/Link.js
+++ todo-app/src/components/Link.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types';
 
 
 const Link = ({ active, children, onClick }) => {
-  if (active) {
-    return <span>{children}</span>
-  }
+    if (active) {
+        return <span>{children}</span>;
+    }
 
-  return (
-    <a
-      href="#"
-      onClick={e => {
-        e.preventDefault()
-        onClick()
-      }}
-    >
-      {children}
-    </a>
-  )
-}
+    return (
+        <a
+          href="#"
+          onClick={e => {
+              e.preventDefault();
+              onClick();
+          }}
+          >
+          {children}
+        </a>
+    );
+};
 
 Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
-}
+    active: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired,
+    onClick: PropTypes.func.isRequired
+};
 
 export default Link;
